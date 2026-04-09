46fd065e563c3a0c065fb948f6ecee8edc61dcab
AdamNur-tech <nikolaj.potapov1999@gmail.com>
2026-04-09 17:05:07 +0200

    Aktualisieren von main.py

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
-# Проверка что сервер работает
 @app.route("/")
 def home():
     return {"status": "VPN backend работает"}
 
-# Получение ссылки
 @app.route("/get-link")
 def get_link():
     device = request.args.get("device")
 
     if device == "android":
         return {"link": "vless://ANDROID_LINK"}
-    
     elif device == "ios":
         return {"link": "vless://IOS_LINK"}
-    
     else:
         return {"error": "unknown device"}
 
-# Запуск сервера (ОБЯЗАТЕЛЬНО В КОНЦЕ)
-app.run(host="0.0.0.0", port=8080)
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
